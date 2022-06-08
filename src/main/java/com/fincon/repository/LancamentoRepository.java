85142302976800698b4783d90d733c52c084a910
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2022-06-08 04:19:09 -0300

    new metohd

--- src/main/java/com/fincon/repository/LancamentoRepository.java
+++ src/main/java/com/fincon/repository/LancamentoRepository.java
@@ -1,6 +1,9 @@
 package com.fincon.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fincon.model.Lancamento;
@@ -8,4 +11,6 @@ import com.fincon.model.Lancamento;
 @Repository
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
-}
+	@Query(value = "select * from lancamento l order by l.numero_parcela", nativeQuery = true)
+	List<Lancamento> findAllOrderNumeroParcela();
+}	
