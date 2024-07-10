2d0c9bc18f8be620b113b4399c56ba54b8b51ee7
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2024-07-10 17:16:50 -0300

    refactory

--- src/main/java/com/fincon/controller/LancamentoController.java
+++ src/main/java/com/fincon/controller/LancamentoController.java
@@ -1,10 +1,8 @@
 package com.fincon.controller;
 
-import com.fincon.dto.LancamentoDTO;
 import com.fincon.model.Lancamento;
 import com.fincon.service.LancamentoService;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
@@ -24,29 +22,20 @@ import lombok.AllArgsConstructor;
 public class LancamentoController {
 	private LancamentoService lancamentoService;
 
-	@GetMapping
-	@RequestMapping("find-all")
-	public ResponseEntity<List<LancamentoDTO>> findAll() {
-		return ResponseEntity.ok(lancamentoService.findAll());
-	}
-
-	@GetMapping
-	@RequestMapping("find-list-main")
+	@GetMapping("find-list-main")
 	public ResponseEntity<Object> findListMain(@RequestParam("id_usuario") UUID idUsuario,
 			@RequestParam("mes_referencia") int pMesReferencia,
 			@RequestParam("ano_referencia") int pAnoReferencia) {
 		return ResponseEntity.ok(lancamentoService.findListMain(idUsuario, pMesReferencia, pAnoReferencia));
 	}
 
-	@PostMapping
-	@RequestMapping("create")
+	@PostMapping("create")
 	public ResponseEntity<Object> create(@RequestBody Lancamento pLancamento) {
 		lancamentoService.saveOrUpdate(pLancamento);
 		return ResponseEntity.noContent().build();
 	}
 
-	@PostMapping
-	@RequestMapping("delete")
+	@PostMapping("delete")
 	public ResponseEntity<Object> delete(@RequestBody UUID id) {
 		lancamentoService.delete(id);
 		return ResponseEntity.noContent().build();
--- src/main/java/com/fincon/controller/UsuarioController.java
+++ src/main/java/com/fincon/controller/UsuarioController.java
@@ -1,15 +1,14 @@
 package com.fincon.controller;
 
 import com.fincon.dto.UserDTO;
+import com.fincon.model.User;
 import com.fincon.service.UsuarioService;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -20,76 +19,19 @@ public class UsuarioController {
 
 	private UsuarioService usuarioService;
 
-	@GetMapping
-	@RequestMapping("find-all")
+	@GetMapping("find-all")
 	public ResponseEntity<List<UserDTO>> findAll() {
 		return ResponseEntity.ok(usuarioService.findAll());
 	}
 
-	/*
-	 * @GetMapping
-	 * 
-	 * @RequestMapping("find-by-id")
-	 * public ResponseEntity<Object> findById(@RequestParam("id") UUID pId) {
-	 * try {
-	 * return
-	 * ResponseEntity.status(HttpStatus.OK).body(usuarioService.findById(pId));
-	 * } catch (Exception e) {
-	 * return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-	 * .body(new ReturnError(HttpStatus.INTERNAL_SERVER_ERROR,
-	 * HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), e.getMessage(),
-	 * "/api/usuario/findById"));
-	 * }
-	 * }
-	 * 
-	 * @PostMapping
-	 * 
-	 * @RequestMapping("create")
-	 * public ResponseEntity<Object> create(@RequestBody User pUsuario) {
-	 * try {
-	 * return
-	 * ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.save(pUsuario))
-	 * ;
-	 * } catch (Exception e) {
-	 * return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-	 * .body(new ReturnError(HttpStatus.INTERNAL_SERVER_ERROR,
-	 * HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), e.getMessage(),
-	 * "/api/usuario/create"));
-	 * }
-	 * 
-	 * }
-	 * 
-	 * @PostMapping
-	 * 
-	 * @RequestMapping("update")
-	 * public ResponseEntity<Object> update(@RequestBody User pUsuario) {
-	 * try {
-	 * usuarioService.update(pUsuario);
-	 * return ResponseEntity.status(HttpStatus.CREATED)
-	 * .body(true);
-	 * } catch (Exception e) {
-	 * return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-	 * .body(new ReturnError(HttpStatus.INTERNAL_SERVER_ERROR,
-	 * HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), e.getMessage(),
-	 * "/api/usuario/update"));
-	 * }
-	 * 
-	 * }
-	 * 
-	 * @PostMapping
-	 * 
-	 * @RequestMapping("delete")
-	 * public ResponseEntity<Object> delete(@RequestBody UUID id) {
-	 * try {
-	 * usuarioService.delete(id);
-	 * return ResponseEntity.status(HttpStatus.OK).body(null);
-	 * } catch (Exception e) {
-	 * return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-	 * .body(new ReturnError(HttpStatus.INTERNAL_SERVER_ERROR,
-	 * HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), e.getMessage(),
-	 * "/api/usuario/delete"));
-	 * }
-	 * 
-	 * }
-	 */
+	@PostMapping("update")
+	public ResponseEntity<Object> update(@RequestBody User pUsuario) {
+		return ResponseEntity.ok(usuarioService.update(pUsuario));
+	}
+
+	@PostMapping("delete")
+	public ResponseEntity<Object> delete(@RequestBody UUID id) {
+		usuarioService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
