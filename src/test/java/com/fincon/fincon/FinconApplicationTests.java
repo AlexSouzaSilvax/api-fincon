3002677ab1915c8ee950f1cb4dc4ea28b8f41e25
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2024-07-22 22:11:25 -0300

    ajusted credenciais

--- src/test/java/com/fincon/fincon/FinconApplicationTests.java
+++ src/test/java/com/fincon/fincon/FinconApplicationTests.java
@@ -3,11 +3,11 @@ package com.fincon.fincon;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class FinconApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+	@Test
+	void contextLoads() {
+	}
 
 }
