744fd6e0b0b4990b1f7184e4c95a5a0c639d80a5
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2024-08-25 08:24:49 -0300

    add Transactional

--- src/main/java/com/fincon/repository/UserRepository.java
+++ src/main/java/com/fincon/repository/UserRepository.java
@@ -16,8 +16,11 @@ import com.fincon.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+        @Transactional
         UserDetails findByUsername(String username);
 
+        @Transactional
         @Query(value = "select username from Usuario where email = :pEmail", nativeQuery = true)
         String findUsernameByEmail(@Param("pEmail") String pEmail);
 
@@ -26,6 +29,7 @@ public interface UserRepository extends JpaRepository<User, UUID> {
         @Query(value = "update Usuario set password = :pSenha where email = :pEmail", nativeQuery = true)
         int updateSenhaByEmail(@Param("pSenha") String pSenha, @Param("pEmail") String pEmail);
 
+        @Transactional
         @Query(value = "select u.* from Usuario u left join lancamento l on u.id = l.id_usuario where l.mes_referencia = :pMesReferencia and l.ano_referencia = :pAnoReferencia", nativeQuery = true)
         List<User> findUserLancamentoMesAtual(@Param("pMesReferencia") int pMesReferencia,
                         @Param("pAnoReferencia") int pAnoReferencia);
@@ -38,12 +42,15 @@ public interface UserRepository extends JpaRepository<User, UUID> {
                         @Param("pUsername") String pUsername, @Param("pPassword") String pPassword,
                         @Param("pId") UUID pId, @Param("pDataAtualizacao") Date pDataAtualizacao);
 
+        @Transactional
         @Query(value = "select id from Usuario where LOWER(username) = LOWER(:pUsername)", nativeQuery = true)
         String findIdByUsername(@Param("pUsername") String pUsername);
 
+        @Transactional
         @Query(value = "SELECT EXISTS (SELECT 1 FROM Usuario WHERE email = :pEmail ) AS registro_existe", nativeQuery = true)
         boolean existsUserByEmail(@Param("pEmail") String pEmail);
 
+        @Transactional
         @Query(value = "SELECT EXISTS (SELECT 1 FROM Usuario WHERE LOWER(username) = LOWER(:pUsername) ) AS registro_existe", nativeQuery = true)
         boolean existsUserByUsername(@Param("pUsername") String pUsername);
 }
