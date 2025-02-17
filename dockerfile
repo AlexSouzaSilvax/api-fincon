28f0ba9490d0fe8c083bfa2bc1e3035c23997218
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2025-02-17 14:57:41 -0300

    add validator env

--- dockerfile
+++ dockerfile
@@ -5,6 +5,7 @@ ENV FINCON_DATABASE_USERNAME=your_username
 ENV FINCON_DATABASE_PASSWORD=your_password
 ENV FINCON_ENVIO_EMAIL_URL=url_api_envio_email
 ENV FINCON_SECRET_KEY_PASSWORD=secret_key_password
+ENV URL_DEPLOY_WEB=url_deply_web
 VOLUME /tmp
 COPY target/fincon-0.0.1-SNAPSHOT.jar fincon.jar
 ENTRYPOINT ["java","-Duser.timezone=America/Sao_Paulo","-jar","/fincon.jar"]
---
+++ src/main/java/com/fincon/EnvironmentValidator.java
@@ -0,0 +1,30 @@
+package com.fincon;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class EnvironmentValidator {
+
+    private static final List<String> REQUIRED_ENV_VARS = List.of(
+            "FINCON_DATABASE_URL",
+            "FINCON_DATABASE_USERNAME",
+            "FINCON_DATABASE_PASSWORD",
+            "FINCON_ENVIO_EMAIL_URL",
+            "FINCON_SECRET_KEY_PASSWORD",
+            "FINCON_DEPLOY_WEB_URL");
+
+    @PostConstruct
+    public void validateEnvVars() {
+        for (String envVar : REQUIRED_ENV_VARS) {
+            String value = System.getenv(envVar);
+            if (value == null || value.isEmpty()) {
+                throw new IllegalStateException(
+                        "A variável de ambiente " + envVar + " não está configurada corretamente!");
+            }
+        }
+    }
+}
